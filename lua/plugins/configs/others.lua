58266a42660ea31091525170474d74870a677133
Iakov Kosiakov <kalterxad34@gmail.com>
2023-10-02 10:25:33 +0300

    add plugins

--- lua/plugins/configs/others.lua
+++ lua/plugins/configs/others.lua
@@ -63,5 +63,14 @@ M.gitsigns = {
     utils.load_mappings("gitsigns", { buffer = bufnr })
   end,
 }
+-- mx.nnoremap('<leader>gg', '<cmd>LazyGit<cr>', 'LazyGit')
+-- mx.nnoremap('<leader>gc', '<cmd>LazyGitFilter<cr>', 'Project Commits')
+-- mx.nnoremap('<leader>gf', '<cmd>LazyGitFilterCurrentFile<cr>', 'Current File Commits')
+--
+-- vim.cmd([[
+--   if has('nvim') && executable('nvr')
+--     let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+--   endif
+-- ]])
 
 return M
