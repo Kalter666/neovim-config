38e9e7812cfb9d94c098cc005b57369b6a7b0e7a
Iakov Kosiakov <kalterxad34@gmail.com>
2023-10-02 10:47:59 +0300

    add lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,21 @@
+---@type LazySpec
+local M = {
+  'kdheepak/lazygit.nvim',
+  cmd = { 'LazyGit', 'LazyGitFilter', 'LazyGitFilterCurrentFile' }
+}
+
+M.init = function()
+  local mx = require('mapx')
+
+  mx.nnoremap('<leader>gg', '<cmd>LazyGit<cr>', 'LazyGit')
+  mx.nnoremap('<leader>gc', '<cmd>LazyGitFilter<cr>', 'Project Commits')
+  mx.nnoremap('<leader>gf', '<cmd>LazyGitFilterCurrentFile<cr>', 'Current File Commits')
+
+  vim.cmd([[
+    if has('nvim') && executable('nvr')
+      let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+    endif
+  ]])
+end
+
+return M
