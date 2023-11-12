e4d4880eb4f666e0dcb65819a4a4643e054efb32
Iakov Kosiakov <kalterxad34@gmail.com>
2023-11-12 15:21:32 +0300

    disable suggestions

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,14 +4,19 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" } 
+local servers = { "html", "cssls", "tsserver", "clangd" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
     capabilities = capabilities,
+    init_options = {
+      preferences = {
+        disableSuggestions = true,
+      },
+    },
   }
 end
 
--- 
+--
 -- lspconfig.pyright.setup { blabla}
