e4c48522fbefd6581fb26378638becc996942757
Iakov Kosiakov <kalterxad34@gmail.com>
2023-12-08 23:15:51 +0300

    replace null ls with guard

--- lua/custom/configs/guard.lua
+++ lua/custom/configs/guard.lua
@@ -1,18 +1,10 @@
-local guard = require('guard')
-local ft = require('guard.filetype')
+local guard = require "guard"
+local ft = require "guard.filetype"
 
+ft("typescript,javascript,typescriptreact,markdown,html,css"):fmt("prettier"):lint "eslint_d"
 
-ft('typescript,javascript,typescriptreact,markdown,html,css')
-  :fmt('prettier')
-  :lint('eslint_d')
+ft("lua"):fmt("stylua"):lint "selene"
 
-ft('lua'):fmt('lsp')
-        :append('stylua')
-        :lint('selene')
-
-ft('go'):fmt('gofmt');
-
-ft('rust'):fmt('rustfmt');
-
+ft("go"):fmt "gofmt"
 
 return guard
