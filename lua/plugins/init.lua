ed4d860a4a813d315018b2573509722c03e9971c
Iakov Kosiakov <kalterxad34@gmail.com>
2024-01-13 03:57:11 +0300

    remvoe indent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -55,23 +55,7 @@ local default_plugins = {
     end,
   },
 
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    version = "2.20.7",
-    init = function()
-      require("core.utils").lazy_load "indent-blankline.nvim"
-    end,
-    opts = function()
-      return require("plugins.configs.others").blankline
-    end,
-    config = function(_, opts)
-      require("core.utils").load_mappings "blankline"
-      dofile(vim.g.base46_cache .. "blankline")
-      require("indent_blankline").setup(opts)
-    end,
-  },
-
-  {
+    {
     "nvim-treesitter/nvim-treesitter",
     init = function()
       require("core.utils").lazy_load "nvim-treesitter"
@@ -496,9 +480,7 @@ local default_plugins = {
   {
     "shellRaining/hlchunk.nvim",
     event = { "UIEnter" },
-    config = function()
-      require("hlchunk").setup {}
-    end,
+    config = true,
   },
   {
     "ray-x/lsp_signature.nvim",
