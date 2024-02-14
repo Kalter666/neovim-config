90e2fae74568fdf414e1b30160b41c6a21dbf010
Iakov Kosiakov <kalterxad34@gmail.com>
2024-02-15 02:36:20 +0300

    fix: null ls

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -2,7 +2,6 @@ local null_ls = require "null-ls"
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local b = null_ls.builtins
-
 local sources = {
 
   -- webdev stuff
@@ -61,8 +60,9 @@ local sources = {
 }
 
 null_ls.setup {
-  debug = true,
+  debug = false,
   sources = sources,
+  debounce = 500,
   -- on_attach = function(client, bufnr)
   --   if client.supports_method "textDocument/formatting" then
   --     vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
